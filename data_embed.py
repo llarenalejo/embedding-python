c763d896ff05c1e8ba28015cf11534bf823a433e
Jesus Alejandro Molina Llarena <llarenalejo@gmail.com>
2024-09-11 03:20:58 -0500

    documenting solution

--- data_embed.py
+++ data_embed.py
@@ -2,6 +2,7 @@ import sys
 from gpt4all import Embed4All
 import psycopg2 
 
+#receives string to be embedded by gpt4all
 def embed_text(text_to_embed:str):
 
  embedder=Embed4All(device='gpu',model_name='nomic-embed-text-v1.f16.gguf')
